b58a42d379391cc872ae4856b469cf42ee058574
Guido Contreras Woda <guiwoda@gmail.com>
2016-01-09 18:44:43 -0300

    Added coverage for SapiEmitter

--- tests/Http/SapiEmitterTest.php
+++ tests/Http/SapiEmitterTest.php
@@ -42,12 +42,14 @@ namespace Fresco\Tests\Http
 
         public function test_it_emits_a_response_object_when_no_headers_were_sent()
         {
-            self::$sapi->shouldReceive('header')->once()->with('HTTP/7.0 999 Just Because.');
             self::$sapi->shouldReceive('ob_end_flush')->never();
             self::$sapi->shouldReceive('ob_get_level')->twice();
 
-            $this->response->shouldReceive('hasHeader')->with('Content-Length')->andReturn(true);
-            $this->expectNormalOutputBehavior();
+            $this
+                ->withContentLength()
+                ->withStatus()
+                ->withoutHeaders()
+                ->withBody();
 
             $this->emitter->emit($this->response);
         }
@@ -62,13 +64,65 @@ namespace Fresco\Tests\Http
 
         public function test_it_calculates_the_response_size_when_the_content_length_is_not_sent_and_size_is_available()
         {
-            $this->response->shouldReceive('hasHeader')->with('Content-Length')->andReturn(false);
-            $this->response->shouldReceive('size')->andReturn(1337);
-            $this->response->shouldReceive('addHeader')->with('Content-Length', '1337')->once()->andReturnSelf();
-
-            $this->expectNormalOutputBehavior();
+            $this
+                ->withoutContentLength()
+                ->withSize()
+                ->withoutHeaders()
+                ->withBody()
+                ->withStatus();
 
             $this->emitter->emit($this->response);
+        }
+
+        public function test_it_doesnt_set_the_response_size_when_the_content_length_is_not_sent_and_size_is_not_available()
+        {
+            $this
+                ->withoutContentLength()
+                ->withoutSize()
+                ->withoutHeaders()
+                ->withBody()
+                ->withStatus();
+
+            $this->emitter->emit($this->response);
+        }
+
+        public function test_it_emits_all_headers()
+        {
+            $this
+                ->withContentLength()
+                ->withStatus()
+                ->withHeaders()
+                ->withBody();
+
+            $this->emitter->emit($this->response);
+        }
+
+        public function test_it_considers_the_max_buffer_level()
+        {
+            self::$sapi->shouldReceive('ob_end_flush')->once();
+            self::$sapi->shouldReceive('ob_get_level')->times(2)->andReturnValues([4, 3]);
+
+            $this
+                ->withContentLength()
+                ->withStatus()
+                ->withoutHeaders()
+                ->withBody();
+
+            $this->emitter->emit($this->response, 3);
+        }
+
+        public function test_it_shouldnt_end_buffer_if_its_level_is_below_the_given_max()
+        {
+            self::$sapi->shouldReceive('ob_end_flush')->never();
+            self::$sapi->shouldReceive('ob_get_level')->once()->andReturnValues([2]);
+
+            $this
+                ->withContentLength()
+                ->withStatus()
+                ->withoutHeaders()
+                ->withBody();
+
+            $this->emitter->emit($this->response, 3);
         }
 
         protected function tearDown()
@@ -79,15 +133,104 @@ namespace Fresco\Tests\Http
             parent::tearDown();
         }
 
-        private function expectNormalOutputBehavior()
+        /**
+         * @return $this
+         */
+        private function withStatus()
         {
+            self::$sapi->shouldReceive('header')->with('HTTP/7.0 999 Just Because.', true)->once();
+
             $this->response->shouldReceive('reason')->once()->andReturn('Just Because.');
             $this->response->shouldReceive('protocol')->once()->andReturn('7.0');
             $this->response->shouldReceive('status')->once()->andReturn(999);
-            $this->response->shouldReceive('headers')->once()->andReturn([]);
-            $this->response->shouldReceive('body')->once()->andReturn('The Body');
 
-            $this->expectOutputString('The Body');
+            return $this;
+        }
+
+        /**
+         * @return $this
+         */
+        private function withContentLength()
+        {
+            $this->response->shouldReceive('hasHeader')->with('Content-Length')->andReturn(true);
+
+            return $this;
+        }
+
+        /**
+         * @return $this
+         */
+        private function withoutContentLength()
+        {
+            $this->response->shouldReceive('hasHeader')->with('Content-Length')->andReturn(false);
+
+            return $this;
+        }
+
+        /**
+         * @param int $size
+         *
+         * @return $this
+         */
+        private function withSize($size = 1337)
+        {
+            $this->response->shouldReceive('size')->andReturn($size);
+            $this->response->shouldReceive('addHeader')->with('Content-Length', (string) $size)->once()->andReturnSelf();
+
+            return $this;
+        }
+
+        /**
+         * @param int $size
+         *
+         * @return $this
+         */
+        private function withoutSize()
+        {
+            $this->response->shouldReceive('size')->andReturnNull();
+            $this->response->shouldReceive('addHeader')->with('Content-Length', \Mockery::any())->never();
+
+            return $this;
+        }
+
+        /**
+         * @return $this
+         */
+        private function withoutHeaders()
+        {
+            $this->response->shouldReceive('headers')->once()->andReturn([]);
+
+            return $this;
+        }
+
+        /**
+         * @return $this
+         */
+        private function withHeaders()
+        {
+            $this->response->shouldReceive('headers')->once()->andReturn([
+                'content-type' => ['application/awesome'],
+                'accept'       => ['the-truth', 'is-a-band'],
+            ]);
+
+            self::$sapi->shouldReceive('header')->with('Content-Type: application/awesome', true)->once();
+            self::$sapi->shouldReceive('header')->with('Accept: the-truth', true)->once();
+            self::$sapi->shouldReceive('header')->with('Accept: is-a-band', false)->once();
+
+            return $this;
+        }
+
+        /**
+         * @param string $body
+         *
+         * @return $this
+         */
+        private function withBody($body = 'The Body')
+        {
+            $this->response->shouldReceive('body')->once()->andReturn($body);
+            $this->expectOutputString($body);
+
+            return $this;
         }
     }
 }
@@ -101,9 +244,9 @@ namespace Fresco\Http {
         return SapiEmitterTest::$headersSent;
     }
 
-    function header($message)
+    function header($string, $replace = true)
     {
-        return SapiEmitterTest::$sapi ? SapiEmitterTest::$sapi->header($message) : \header($message);
+        return SapiEmitterTest::$sapi ? SapiEmitterTest::$sapi->header($string, $replace) : \header($string, $replace);
     }
 
     function ob_get_level()
