42ef89bd138faae45e555efc2d16ac1fddf1b1c3
Guido Contreras Woda <guiwoda@gmail.com>
2016-01-12 21:42:44 -0300

    RouteFileDefinition test

---
+++ tests/Definitions/RouteFileDefinitionTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Fresco\Tests\Definitions;
+
+use Fresco\Contracts\Routing\RouteLoader;
+use Fresco\Definitions\RouteFileDefinition;
+use Interop\Container\Definition\DefinitionProviderInterface;
+
+class RouteFileDefinitionTest extends DefinitionTestCase
+{
+    public function getDefinition() : DefinitionProviderInterface
+    {
+        return new RouteFileDefinition();
+    }
+
+    public function shouldDefine() : array
+    {
+        return [
+            RouteLoader::class
+        ];
+    }
+}
