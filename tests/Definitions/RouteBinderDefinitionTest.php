a99df9f0cf43d09b1bdda28c515d433e627c8869
Guido Contreras Woda <guiwoda@gmail.com>
2016-01-12 21:41:19 -0300

    RouteBinderDefinition test

---
+++ tests/Definitions/RouteBinderDefinitionTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Fresco\Tests\Definitions;
+
+use Fresco\Contracts\Routing\RouteLoader;
+use Fresco\Definitions\RouteBinderDefinition;
+use Interop\Container\Definition\DefinitionProviderInterface;
+
+class RouteBinderDefinitionTest extends DefinitionTestCase
+{
+    public function getDefinition() : DefinitionProviderInterface
+    {
+        return new RouteBinderDefinition();
+    }
+
+    public function shouldDefine() : array
+    {
+        return [
+            RouteLoader::class
+        ];
+    }
+}
